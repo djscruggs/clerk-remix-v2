9aa90c7d50b222719af71c28a5751fde38798564
Derek Scruggs <me@derekscruggs.com>
2024-05-18 14:21:31 -0500

    reproduction of bug -- culprit appears to be ClerkProvider

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/remix";
+import { SignedIn, SignedOut, SignInButton, UserButton, SignIn } from "@clerk/remix";
 import type { MetaFunction } from "@remix-run/node";
+import { ClerkProvider} from '@clerk/clerk-react'
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,21 +10,25 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const key = 'pk_test_b3B0aW11bS1yYXktODcuY2xlcmsuYWNjb3VudHMuZGV2JA'
   return (
-    <div
-      style={{
-        fontFamily: "system-ui, sans-serif",
-        lineHeight: "1.4",
-        textAlign: "center",
-      }}
-    >
-      <h1>Hello Clerk!</h1>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-    </div>
+    <ClerkProvider  publishableKey={key} signInUrl='/sign-in'>
+      <div
+        style={{
+          fontFamily: "system-ui, sans-serif",
+          lineHeight: "1.4",
+          textAlign: "center",
+        }}
+      >
+        <h1>Hello Clerk!</h1>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton />
+          <SignIn />
+        </SignedOut>
+      </div>
+    </ClerkProvider>
   );
 }
